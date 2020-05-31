fae4b50cefa5ce3c72ba3b3a80259e471ccebdcb
Christian <christianhandest@gmail.com>
2020-05-31 19:09:42 +0200

    pulished

--- InternalHack/dllmain.cpp
+++ InternalHack/dllmain.cpp
@@ -1,32 +1,74 @@
+#include <iostream>
 #include "windows.h"
+#include "Keys.h"
 
-DWORD WINAPI InternalHack(HINSTANCE);
+DWORD WINAPI Hack(HMODULE hModule)
+{
+    //Create Console
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
 
-BOOL APIENTRY DllMain(HINSTANCE moduleHandle, DWORD  ul_reason_for_call, LPVOID lpReserved)
+    std::cout << "\n======================  INTERNAL HACK INJECTED  ======================" << std::endl;
+    std::cout << "Press 'X'         Activate ammunition hack" << std::endl;
+
+    std::cout << "Press 'C'         Detach hack from process" << std::endl;
+    std::cout << "======================================================================" << "\n" << std::endl;
+
+    uintptr_t pModuleBase = (uintptr_t)GetModuleHandle(L"ac_client.exe");
+
+    while (true)
+    {
+        if (GetAsyncKeyState(KeyPress::VK_C) & 1)
+        {
+            std::cout << "Detaching... " << std::endl;
+            Sleep(3000);
+            break;
+        }
+
+        if (GetAsyncKeyState(KeyPress::VK_X) & 1)
+        {
+            // moduleBase: 400000, playerbase ptr: moduleBase + 0x10F4F4
+            std::cout << "moduleBase: " << std::uppercase << std::hex << pModuleBase << std::endl;
+
+            // Address 0x50F4F4
+            std::cout << "localPlayer pointer (moduleBase + 0x10F4F4)" << std::uppercase << std::hex << (pModuleBase + 0x10F4F4) << std::endl;
+
+            // 0x50F4F4 contains a ptr, pointing to playerbase.
+            uintptr_t pBaseAddress = *(uintptr_t*)(pModuleBase + 0x10F4F4);
+
+            std::cout << "pBaseAddress: " << std::uppercase << std::hex << (pBaseAddress) << std::endl;
+
+            int offsetAmmo = 0x150;
+
+            uintptr_t pAmmo = pBaseAddress + offsetAmmo;
+            std::cout << "pAmmo: " << std::uppercase << std::hex << (pAmmo) << std::endl;
+
+            int* ammo = (int*)pAmmo;
+            *ammo = 1337;
+        }
+        Sleep(100);
+    }
+
+    fclose(f);
+    FreeConsole();
+    FreeLibraryAndExitThread(hModule, 0);
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)InternalHack, moduleHandle, 0, nullptr));
+        CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Hack, hModule, 0, 0));
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
         break;
     }
     return TRUE;
-}
-
-DWORD WINAPI InternalHack(HINSTANCE moduleHandle)
-{
-    int offsetAmmo = 0x150;
-    int baseOffset = 0x10F4F4;
-
-    uintptr_t pModuleBase = (uintptr_t)GetModuleHandle(L"ac_client.exe");
-    uintptr_t pBaseAddress = *(uintptr_t*)(pModuleBase + baseOffset);
-    uintptr_t pAmmo = pBaseAddress + offsetAmmo;
-    int* ammo = (int*)pAmmo;
-    *ammo = 1337;
-
-    FreeLibraryAndExitThread(moduleHandle, 0);
-    return 0;
 }
